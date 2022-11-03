9dafb2481631c497a65a6c82f7cc9b4936653d48
Steve Kemp <steve@steve.org.uk>
2022-11-03 05:04:17 +0200

    Seperately handle types.
    We now sort between "raw functions" and those with receivers.

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -4,6 +4,7 @@ package analyzer
 
 import (
 	"go/ast"
+	"go/token"
 
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
@@ -30,38 +31,145 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		(*ast.FuncDecl)(nil),
 	}
 
-	// The previous function name, on a per-file basis
-	prev := make(map[string]string)
+	// We want to differentiate between function calls that have
+	// receivers, and those that don't
+	type Function struct {
+		// The file containing the function
+		File string
 
+		// The name of the function.
+		Name string
+
+		// The position within our source where this object was.
+		Position token.Pos
+
+		// The receiver, if any, for the function
+		Receiver string
+	}
+
+	// Create a list of all the functions we've found
+	seen := []*Function{}
+
+	// Now update that list with all the function definitions
+	// we encounter, in order.
 	inspector.Preorder(nodeFilter, func(node ast.Node) {
 
 		// Get the node, in the right type
 		funcDecl := node.(*ast.FuncDecl)
 
-		// Get the name of the function
-		name := funcDecl.Name.Name
+		// Build up any (optional) receiver
+		recv := ""
 
-		// Get the name of the file we're analyzing
-		//
-		// Since we analyze based on packages, but packages
-		// may contain multiple source files
-		file := pass.Fset.File(node.Pos()).Name()
+		// Is there a receiver?
+		if funcDecl.Recv != nil {
 
-		// ignore special cases
-		if ( name == "" || name == "init" || name == "main" ) {
-			return
+			for _, x := range funcDecl.Recv.List {
+
+				if x.Names != nil {
+					for _, y := range x.Names {
+						recv += y.Name
+					}
+				}
+			}
 		}
 
-		// Is this function out of order?
-		if name != "" && ( name < prev[file] ) {
-
-			pass.Reportf(node.Pos(),
-				"function %s should have been before %s",
-				name, prev[file])
+		// The entry for the function, with appropriate data
+		tmp := &Function{
+			File:     pass.Fset.File(node.Pos()).Name(),
+			Name:     funcDecl.Name.Name,
+			Position: node.Pos(),
+			Receiver: recv,
 		}
 
-		prev[file] = name
+		// Save it away
+		seen = append(seen, tmp)
 	})
+
+	//
+	// Now we've processed all the functions.
+	//
+	// We want to build up a list of files to operate upon
+	// then a list of functions.
+	//
+	files := make(map[string]bool)
+	for _, fnc := range seen {
+
+		files[fnc.File] = true
+	}
+
+	//
+	// Now we have a list of unique filenames
+	//
+	// Process each one
+	//
+	for fn := range files {
+
+		//
+		// Function names we've seen that have a receiver
+		//
+		obj := make(map[string][]*Function)
+
+		//
+		// Function names with no receiver
+		//
+		raw := []*Function{}
+
+		//
+		// Append the function for this file
+		// to the appropriate slice
+		//
+		for _, ent := range seen {
+			if ent.File != fn {
+				continue
+			}
+
+			//
+			// Record the method, with either a receiver
+			// or not.
+			//
+			if ent.Receiver == "" {
+				raw = append(raw, ent)
+			} else {
+				obj[ent.Receiver] = append(obj[ent.Receiver], ent)
+			}
+		}
+
+		//
+		// Now we have a list of methods with receivers
+		// and not
+		//
+
+		//
+		// Test each receiver
+		//
+		seen := make(map[string]*Function)
+		for _, entries := range obj {
+
+			for _, ent := range entries {
+
+				if (seen[ent.Receiver] != nil) && (ent.Name < seen[ent.Receiver].Name) {
+					pass.Reportf(ent.Position,
+						"function %s%s should have been before %s%s",
+						ent.Receiver, ent.Name, seen[ent.Receiver].Receiver, seen[ent.Receiver].Name)
+				}
+				seen[ent.Receiver] = ent
+			}
+		}
+
+		//
+		// Then test those functions with no receiver
+		//
+		prev := ""
+
+		for _, r := range raw {
+			if (prev != "") && (r.Name < prev) {
+				pass.Reportf(r.Position,
+					"function %s should have been before %s",
+					r.Name, prev)
+			}
+			prev = r.Name
+		}
+	}
 
 	return nil, nil
 }
